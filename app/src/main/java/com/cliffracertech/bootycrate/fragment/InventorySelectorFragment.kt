16cd9489943c30791e2b245c166bd710132c3b09
NicholasHochstetler <NicholasHochstetler@gmail.com>
2021-10-03 16:05:37 -0400

    Update inventory view to be expandable and show extra fields.

--- app/src/main/java/com/cliffracertech/bootycrate/fragment/InventorySelectorFragment.kt
+++ app/src/main/java/com/cliffracertech/bootycrate/fragment/InventorySelectorFragment.kt
@@ -13,8 +13,10 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.view.ViewPropertyAnimator
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.DiffUtil
@@ -26,7 +28,10 @@ import com.cliffracertech.bootycrate.database.BootyCrateInventory
 import com.cliffracertech.bootycrate.database.inventoryViewModel
 import com.cliffracertech.bootycrate.databinding.InventorySelectorBinding
 import com.cliffracertech.bootycrate.databinding.InventoryViewBinding
+import com.cliffracertech.bootycrate.recyclerview.ExpandableItemAnimator
 import com.cliffracertech.bootycrate.recyclerview.ItemSpacingDecoration
+import com.cliffracertech.bootycrate.utils.AnimatorConfig
+import com.cliffracertech.bootycrate.utils.applyConfig
 import com.cliffracertech.bootycrate.utils.dpToPixels
 import java.util.*
 
@@ -55,6 +60,7 @@ class InventorySelectorRecyclerView(context: Context, attrs: AttributeSet) :
 {
     private val viewModel = inventoryViewModel(context)
     private val adapter = Adapter()
+    private val animator = ExpandableItemAnimator(AnimatorConfig.appDefault(context))
 
     init {
         setAdapter(adapter)
@@ -62,6 +68,7 @@ class InventorySelectorRecyclerView(context: Context, attrs: AttributeSet) :
         layoutManager = LinearLayoutManager(context)
         val spacing = resources.getDimensionPixelSize(R.dimen.recycler_view_item_spacing)
         addItemDecoration(ItemSpacingDecoration(spacing))
+        itemAnimator = animator
     }
 
     fun observeViewModel(owner: LifecycleOwner) =
@@ -93,6 +100,11 @@ class InventorySelectorRecyclerView(context: Context, attrs: AttributeSet) :
                     ui.shoppingListItemCountView.text = item.shoppingListItemCount.toString()
                 if (changes.contains(Field.InventoryItemCount))
                     ui.inventoryItemCountView.text = item.inventoryItemCount.toString()
+                if (changes.contains(Field.IsExpanded))
+                    holder.view.setExpanded(item.isExpanded)
+                if (changes.contains(Field.UseInCombinedShoppingList) &&
+                    ui.useInCombinedShoppingListSwitch.isChecked != item.useInCombinedShoppingList)
+                        ui.useInCombinedShoppingListSwitch.isChecked = item.useInCombinedShoppingList
             }
         }
 
@@ -105,10 +117,19 @@ class InventorySelectorRecyclerView(context: Context, attrs: AttributeSet) :
 
         init {
             view.ui.nameView.onTextChangedListener = { viewModel.updateName(item.id, it) }
+            view.ui.editButton.setOnClickListener {
+                if (adapterPosition != -1)
+                    viewModel.setExpandedItem(if (view.isExpanded) null else item.id)
+            }
+            view.ui.useInCombinedShoppingListSwitch.setOnCheckedChangeListener { _, isChecked ->
+                if (adapterPosition != -1)
+                    viewModel.updateUseInCombinedShoppingList(item.id, isChecked)
+            }
         }
     }
 
-    private enum class Field { Name, ShoppingListItemCount, InventoryItemCount }
+    private enum class Field { Name, ShoppingListItemCount, InventoryItemCount,
+                               IsExpanded, UseInCombinedShoppingList }
 
     private class DiffUtilCallback : DiffUtil.ItemCallback<BootyCrateInventory>() {
         private val listChanges = mutableMapOf<Long, EnumSet<Field>>()
@@ -120,11 +141,16 @@ class InventorySelectorRecyclerView(context: Context, attrs: AttributeSet) :
         override fun areContentsTheSame(oldItem: BootyCrateInventory, newItem: BootyCrateInventory) =
             itemChanges.apply {
                 clear()
-                if (newItem.name != oldItem.name) add(Field.Name)
+                if (newItem.name != oldItem.name)
+                    add(Field.Name)
                 if (newItem.shoppingListItemCount != oldItem.shoppingListItemCount)
                     add(Field.ShoppingListItemCount)
                 if (newItem.inventoryItemCount != oldItem.inventoryItemCount)
                     add(Field.InventoryItemCount)
+                if (newItem.isExpanded != oldItem.isExpanded)
+                    add(Field.IsExpanded)
+                if (newItem.useInCombinedShoppingList != oldItem.useInCombinedShoppingList)
+                    add(Field.UseInCombinedShoppingList)
 
                 if (!isEmpty())
                     listChanges[newItem.id] = EnumSet.copyOf(this)
@@ -135,8 +161,17 @@ class InventorySelectorRecyclerView(context: Context, attrs: AttributeSet) :
     }
 }
 
-class InventoryView(context: Context) : LinearLayout(context) {
+class InventoryView(context: Context) : ConstraintLayout(context),
+    ExpandableItemAnimator.ExpandableRecyclerViewItem
+{
     val ui = InventoryViewBinding.inflate(LayoutInflater.from(context), this)
+    private val pendingAnimators = mutableListOf<ViewPropertyAnimator>()
+    var isExpanded = false
+        private set
+
+    var animatorConfig: AnimatorConfig? = null
+        set(value) { field = value
+                     ui.nameView.animatorConfig = value }
 
     init {
         val inventoryIcon = ContextCompat.getDrawable(context, R.drawable.inventory_icon)
@@ -156,5 +191,42 @@ class InventoryView(context: Context) : LinearLayout(context) {
         ui.nameView.setText(inventory.name)
         ui.shoppingListItemCountView.text = inventory.shoppingListItemCount.toString()
         ui.inventoryItemCountView.text = inventory.inventoryItemCount.toString()
+    }
+
+    override fun setExpanded(expanding: Boolean, animate: Boolean) {
+        isExpanded = expanding
+        ui.nameView.setEditable(expanding, animate)
+        if (!animate) {
+            ui.deleteButton.isVisible = expanding
+            ui.useInCombinedShoppingListSwitch.isVisible = expanding
+            return
+        }
+
+        ui.deleteButton.isVisible = true
+        ui.deleteButton.alpha = if (expanding) 0f else 1f
+        pendingAnimators.add(ui.deleteButton.run {
+            animate().alpha(if (expanding) 1f else 0f).applyConfig(animatorConfig).withLayer()
+                .withEndAction { if (alpha == 0f) ui.deleteButton.isVisible = false }
+        })
+
+        ui.useInCombinedShoppingListSwitch.isVisible = true
+        ui.useInCombinedShoppingListSwitch.alpha = if (expanding) 0f else 1f
+        pendingAnimators.add(ui.useInCombinedShoppingListSwitch.run {
+            animate().alpha(if (expanding) 1f else 0f).applyConfig(animatorConfig)
+                .withLayer().withEndAction { if (alpha == 0f) isVisible = false }
+        })
+
+        ui.editButton.isActivated = expanding
+        val editButtonTransYAmount = ui.editButton.layoutParams.height
+        if (expanding)
+            ui.editButton.translationY = -1f * editButtonTransYAmount
+        val editButtonEndTranslation = if (expanding) 0f else -editButtonTransYAmount.toFloat()
+        pendingAnimators.add(ui.editButton.animate().applyConfig(animatorConfig)
+                            .translationY(editButtonEndTranslation).withLayer())
+    }
+
+    override fun runPendingAnimations() {
+        pendingAnimators.forEach { it.start() }
+        pendingAnimators.clear()
     }
 }
