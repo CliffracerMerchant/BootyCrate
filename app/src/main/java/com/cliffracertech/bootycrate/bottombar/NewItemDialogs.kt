88d475a1b86e6f48025529c7d8ed2f3be5d84f62
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-02-13 12:51:47 -0500

    Add message display to new item dialogs Fix NewShoppingListItemDialog

--- app/src/main/java/com/cliffracertech/bootycrate/bottombar/NewItemDialogs.kt
+++ app/src/main/java/com/cliffracertech/bootycrate/bottombar/NewItemDialogs.kt
@@ -4,20 +4,31 @@
  * or in the file LICENSE in the project's root directory. */
 package com.cliffracertech.bootycrate.bottombar
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cliffracertech.bootycrate.R
 import com.cliffracertech.bootycrate.itemlist.InventoryItemView
@@ -27,6 +38,8 @@ import com.cliffracertech.bootycrate.itemlist.shoppingListItemCallback
 import com.cliffracertech.bootycrate.model.database.InventoryItem
 import com.cliffracertech.bootycrate.model.database.ListItem
 import com.cliffracertech.bootycrate.model.database.ShoppingListItem
+import com.cliffracertech.bootycrate.model.database.Validator
+import kotlinx.collections.immutable.ImmutableList
 
 /**
  * A dialog to add new [ListItem] subclasses.
@@ -38,6 +51,8 @@ import com.cliffracertech.bootycrate.model.database.ShoppingListItem
  * @param onAddAnotherClick The callback that will be invoked when
  *     the add another button is clicked
  * @param onOkClick The callback that will be invoked when the ok button is clicked
+ * @param messages An [ImmutableList]`<Validator.Message>` of messages to display
+ *     regarding the current new item data (e.g. why the current name is invalid
  * @param modifier The [Modifier] to use for the dialog
  * @param newItemView A composable lambda that contains the new item view
  */
@@ -46,6 +61,7 @@ import com.cliffracertech.bootycrate.model.database.ShoppingListItem
     confirmButtonsEnabled: Boolean,
     onAddAnotherClick: () -> Unit,
     onOkClick: () -> Unit,
+    messages: ImmutableList<Validator.Message>,
     modifier: Modifier = Modifier,
     newItemView: @Composable () -> Unit
 ) {
@@ -58,6 +74,7 @@ import com.cliffracertech.bootycrate.model.database.ShoppingListItem
         text = { Column {
             newItemView()
             // itemGroupPicker
+            ValidatorMessageList(messages)
         }},
         buttons = { Row {
             // Cancel button
@@ -84,70 +101,96 @@ import com.cliffracertech.bootycrate.model.database.ShoppingListItem
  * to create new [ShoppingListItem]s. See [NewItemDialog] for parameter descriptions. */
 @Composable fun NewShoppingListItemDialog(
     onDismissRequest: () -> Unit,
-    confirmButtonsEnabled: Boolean,
     onAddAnotherClick: () -> Unit,
     onOkClick: () -> Unit,
+    messages: ImmutableList<Validator.Message>,
     modifier: Modifier = Modifier,
-) = NewItemDialog(
-    onDismissRequest, confirmButtonsEnabled,
-    onAddAnotherClick, onOkClick, modifier
 ) {
     val viewModel: NewShoppingListItemDialogViewModel = viewModel()
-    var color by rememberSaveable { mutableStateOf(ListItem.Color.values().first())}
-    var amount by rememberSaveable { mutableStateOf(1) }
 
-    val callback = remember {
-        shoppingListItemCallback(
-            onColorChangeRequest = { color = it },
-            onRenameRequest = { viewModel.itemName = it },
-            onExtraInfoChangeRequest = { viewModel.itemExtraInfo = it },
-            onAmountChangeRequest = { amount = it },
-            showEditButton = false)
+    NewItemDialog(
+        onDismissRequest, viewModel.confirmButtonsEnabled,
+        onAddAnotherClick, onOkClick, messages, modifier
+    ) {
+        ShoppingListItemView(
+            viewModel.itemColorIndex,
+            viewModel.itemName,
+            viewModel.itemExtraInfo,
+            viewModel.itemAmount,
+            viewModel.itemIsChecked,
+            callback = remember {
+                shoppingListItemCallback(
+                    onColorChangeRequest = { viewModel.itemColorIndex = it.ordinal },
+                    onRenameRequest = { viewModel.itemName = it },
+                    onExtraInfoChangeRequest = { viewModel.itemExtraInfo = it },
+                    onAmountChangeRequest = { viewModel.itemAmount = it },
+                    showEditButton = false)
+            })
     }
-
-    ShoppingListItemView(
-        viewModel.itemColorIndex,
-        viewModel.itemName,
-        viewModel.itemExtraInfo,
-        viewModel.itemAmount,
-        viewModel.itemIsChecked,
-        callback)
 }
 
 /** A [NewItemDialog] that contains an [InventoryItemView] to allow the user to
  * create new [InventoryItem]s. See [NewItemDialog] for parameter descriptions. */
 @Composable fun NewInventoryItemDialog(
     onDismissRequest: () -> Unit,
-    confirmButtonsEnabled: Boolean,
     onAddAnotherClick: () -> Unit,
     onOkClick: () -> Unit,
+    messages: ImmutableList<Validator.Message>,
     modifier: Modifier = Modifier,
-) = NewItemDialog(
-    onDismissRequest, confirmButtonsEnabled,
-    onAddAnotherClick, onOkClick, modifier
 ) {
     val viewModel: NewInventoryItemDialogViewModel = viewModel()
 
-    val callback = remember {
-        inventoryItemCallback(
-            onColorChangeRequest = { viewModel.itemColorIndex = it.ordinal },
-            onRenameRequest = { viewModel.itemName = it },
-            onExtraInfoChangeRequest = { viewModel.itemExtraInfo = it },
-            onAmountChangeRequest = { viewModel.itemAmount = it },
-            showEditButton = false,
-            onAutoAddToShoppingListCheckboxClick = {
-                viewModel.itemAutoAddToShoppingList = !viewModel.itemAutoAddToShoppingList
-            }, onAutoAddToShoppingListAmountChangeRequest = {
-                viewModel.itemAutoAddToShoppingListAmount = it
+    NewItemDialog(
+        onDismissRequest, viewModel.confirmButtonsEnabled,
+        onAddAnotherClick, onOkClick, messages, modifier
+    ) {
+        InventoryItemView(
+            viewModel.itemColorIndex,
+            viewModel.itemName,
+            viewModel.itemExtraInfo,
+            viewModel.itemAmount,
+            viewModel.itemAutoAddToShoppingList,
+            viewModel.itemAutoAddToShoppingListAmount,
+            callback = remember {
+                inventoryItemCallback(
+                    onColorChangeRequest = { viewModel.itemColorIndex = it.ordinal },
+                    onRenameRequest = { viewModel.itemName = it },
+                    onExtraInfoChangeRequest = { viewModel.itemExtraInfo = it },
+                    onAmountChangeRequest = { viewModel.itemAmount = it },
+                    showEditButton = false,
+                    onAutoAddToShoppingListCheckboxClick = {
+                        viewModel.itemAutoAddToShoppingList = !viewModel.itemAutoAddToShoppingList
+                    }, onAutoAddToShoppingListAmountChangeRequest = {
+                        viewModel.itemAutoAddToShoppingListAmount = it
+                    })
             })
     }
+}
 
-    InventoryItemView(
-        viewModel.itemColorIndex,
-        viewModel.itemName,
-        viewModel.itemExtraInfo,
-        viewModel.itemAmount,
-        viewModel.itemAutoAddToShoppingList,
-        viewModel.itemAutoAddToShoppingListAmount,
-        callback)
+@Composable fun ValidatorMessageList(
+    messages: ImmutableList<Validator.Message>,
+    modifier: Modifier = Modifier
+) {
+    val messageHeight = 48.dp
+    val context = LocalContext.current
+    LazyColumn(modifier.animateContentSize()) {
+        items(messages) {
+            Row(Modifier.fillMaxWidth().height(messageHeight),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val vector = when {
+                    it.isInformational -> Icons.Default.Info
+                    it.isWarning ->       Icons.Default.Warning
+                    else ->/*it.isError*/ Icons.Default.Error
+                }
+                val tint = when {
+                    it.isInformational -> Color.Blue
+                    it.isWarning ->       Color.Yellow
+                    else ->/*it.isError*/ MaterialTheme.colors.error
+                }
+                Icon(vector, null, tint = tint)
+                Text(it.stringResource.resolve(context))
+            }
+        }
+    }
 }
