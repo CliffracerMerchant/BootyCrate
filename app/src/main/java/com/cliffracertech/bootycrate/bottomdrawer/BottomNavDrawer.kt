8d612cf99635a8259393a2ced7488929ee26afbc
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-03-06 15:17:34 -0500

    Add dialogs to BootyCrateBottomNavDrawer

--- app/src/main/java/com/cliffracertech/bootycrate/bottomdrawer/BottomNavDrawer.kt
+++ app/src/main/java/com/cliffracertech/bootycrate/bottomdrawer/BottomNavDrawer.kt
@@ -7,29 +7,23 @@ package com.cliffracertech.bootycrate.bottomdrawer
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.material.AlertDialog
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -44,8 +38,9 @@ import com.cliffracertech.bootycrate.model.database.ItemGroup
 import com.cliffracertech.bootycrate.model.database.ItemGroupDao
 import com.cliffracertech.bootycrate.model.database.ItemGroupNameValidator
 import com.cliffracertech.bootycrate.model.database.SettingsDao
-import com.cliffracertech.bootycrate.model.database.Validator
-import com.cliffracertech.bootycrate.ui.SingleValidatorMessage
+import com.cliffracertech.bootycrate.ui.ConfirmDialog
+import com.cliffracertech.bootycrate.ui.NameDialog
+import com.cliffracertech.bootycrate.ui.NameDialogState
 import com.cliffracertech.bootycrate.utils.collectAsState
 import com.cliffracertech.bootycrate.utils.toPx
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -98,40 +93,44 @@ import javax.inject.Inject
 
 
     private val itemGroupNameValidator = ItemGroupNameValidator(itemGroupDao)
-
-    val itemGroupNameDialogMessage by
+    private val itemGroupNameDialogMessage by
         itemGroupNameValidator.message.collectAsState(null, scope)
 
-    var itemGroupNameDialogTarget by mutableStateOf<ItemGroup?>(null)
+    private var renameDialogTargetId by mutableStateOf<Long?>(null)
+    private fun hideRenameDialog() {
+        renameDialogState = NameDialogState.NotShowing
+        itemGroupNameValidator.clear()
+    }
+    private val renameDialogShowingState = NameDialogState.Showing(
+        currentNameProvider = itemGroupNameValidator::value::get,
+        messageProvider = { itemGroupNameDialogMessage },
+        onNameChange = itemGroupNameValidator::value::set,
+        onCancel = ::hideRenameDialog,
+        onConfirm = {
+            scope.launch {
+                val validatedName = itemGroupNameValidator.validate()
+                val id = renameDialogTargetId
+                if (validatedName == null || id == null)
+                    return@launch
+                itemGroupDao.updateName(id, validatedName)
+                hideRenameDialog()
+            }
+        })
+
+    var renameDialogState by mutableStateOf<NameDialogState>(NameDialogState.NotShowing)
         private set
 
     fun onItemGroupRenameClick(itemGroup: ItemGroup) {
-        itemGroupNameDialogTarget = itemGroup
-    }
-
-    fun onItemGroupNameDialogCancel() {
-        itemGroupNameDialogTarget = null
-        itemGroupNameValidator.clear()
-    }
-
-    fun onItemGroupNameDialogChange(newName: String) {
-        itemGroupNameValidator.value = newName
-    }
-
-    fun onItemGroupRenameDialogConfirm() {
-        scope.launch {
-            val validatedName = itemGroupNameValidator.validate()
-            val id = itemGroupNameDialogTarget?.id
-            if (validatedName == null || id == null)
-                return@launch
-            itemGroupDao.updateName(id, validatedName)
-            onItemGroupNameDialogCancel()
-        }
+        renameDialogState = renameDialogShowingState
+        renameDialogTargetId = itemGroup.id
     }
 
 
-    var confirmDeleteDialogTarget by mutableStateOf<ItemGroup?>(null)
-        private set
+    private var confirmDeleteDialogTarget by mutableStateOf<ItemGroup?>(null)
+
+    val showingConfirmDeleteDialog by derivedStateOf {
+        confirmDeleteDialogTarget != null
+    }
 
     fun onItemGroupDeleteClick(itemGroup: ItemGroup) {
         confirmDeleteDialogTarget = itemGroup
@@ -148,28 +147,29 @@ import javax.inject.Inject
     }
 
 
-    var showingNewItemGroupDialog by mutableStateOf(false)
+    private fun hideNewItemGroupDialog() {
+        newItemGroupDialogState = NameDialogState.NotShowing
+        itemGroupNameValidator.clear()
+    }
+    private val newItemGroupDialogShowingState = NameDialogState.Showing(
+        currentNameProvider = itemGroupNameValidator::value::get,
+        messageProvider = { itemGroupNameDialogMessage },
+        onNameChange = itemGroupNameValidator::value::set,
+        onCancel = ::hideNewItemGroupDialog,
+        onConfirm = {
+            scope.launch {
+                val validatedName = itemGroupNameValidator.validate()
+                    ?: return@launch
+                itemGroupDao.add(validatedName)
+                hideNewItemGroupDialog()
+            }
+        })
+
+    var newItemGroupDialogState by mutableStateOf<NameDialogState>(NameDialogState.NotShowing)
         private set
 
     fun onAddButtonClick() {
-        showingNewItemGroupDialog = true
-    }
-
-    fun onNewItemGroupDialogNameChange(newName: String) {
-        itemGroupNameValidator.value = newName
-    }
-
-    fun onNewItemGroupDialogCancel() {
-        showingNewItemGroupDialog = false
-    }
-
-    fun onNewItemGroupDialogConfirm() {
-        scope.launch {
-            val validatedName = itemGroupNameValidator.validate()
-                ?: return@launch
-            itemGroupDao.add(validatedName)
-            onNewItemGroupDialogCancel()
-        }
+        newItemGroupDialogState = newItemGroupDialogShowingState
     }
 }
 
@@ -228,75 +228,37 @@ enum class DrawerState { Hidden, Collapsed, Expanded;
                 })
         }
     }
+
+    RenameItemGroupDialog(vm.renameDialogState)
+    NewItemGroupDialog(vm.newItemGroupDialogState)
+
+    if (vm.showingConfirmDeleteDialog)
+        ConfirmDialog(
+            modifier = modifier,
+            message = stringResource(
+                R.string.confirm_delete_item_group_message),
+            onDismissRequest = vm::onConfirmDeleteDialogCancelClick,
+            onConfirm = vm::onConfirmDeleteDialogConfirmClick)
 }
 
-@Composable fun CancelOkButtonRow(
+@Composable private fun RenameItemGroupDialog(
+    state: NameDialogState,
     modifier: Modifier = Modifier,
-    okButtonEnabled: Boolean = true,
-    onCancelClick: () -> Unit,
-    onOkClick: () -> Unit,
-) = Row(modifier) {
-    Spacer(Modifier.weight(1f))
-    TextButton(onClick = onCancelClick) {
-        Text(stringResource(android.R.string.cancel))
-    }
-    TextButton(
-        onClick = onOkClick,
-        enabled = okButtonEnabled
-    ) {
-        Text(stringResource(android.R.string.ok))
-    }
+) {
+    if (state !is NameDialogState.Showing) return
+
+    val title = stringResource(
+        R.string.rename_item_group_dialog_title,
+        state.currentNameProvider())
+    NameDialog(title, state, modifier)
 }
 
-/**
- * A naming dialog for an [ItemGroup].
- *
- * @param modifier The [Modifier] to use for the root layout
- * @param currentName The current name of the [ItemGroup] whose name
- *     is being set. The title of the dialog will change according to
- *     whether this value starts as a blank string.
- * @param onCurrentNameChange The callback that will be invoked when
- *     the current name is changed
- * @param message A nullable [Validator.Message] that provides information about
- *     the current value of [currentName] (e.g. that it is invalid) if necessary
- * @param onDismissRequest The callback that will be invoked when the user tries
- *     to dismiss the dialog via the cancel button, the back button/gesture, or
- *     a click outside the dialog
- * @param onConfirm The callback that will be invoked when the user clicks the ok button
- */
-@Composable private fun ItemGroupNameDialog(
+@Composable private fun NewItemGroupDialog(
+    state: NameDialogState,
     modifier: Modifier = Modifier,
-    currentName: String,
-    onCurrentNameChange: (String) -> Unit,
-    message: Validator.Message?,
-    onDismissRequest: () -> Unit,
-    onConfirm: () -> Unit,
-) = AlertDialog(
-    modifier = modifier,
-    onDismissRequest = onDismissRequest,
-    title = {
-        val context = LocalContext.current
-        val title = remember {
-            if (currentName.isBlank())
-                context.getString(R.string.add_item_group_dialog_title)
-            else context.getString(R.string.rename_item_group_dialog_title, currentName)
-        }
-        Text(title)
-    }, text = {
-        Column {
-            TextField(
-                onValueChange = onCurrentNameChange,
-                value = currentName,
-                modifier = Modifier.fillMaxWidth(),
-//                    .padding(horizontal = 16.dp),
-                isError = message is Validator.Message.Error,
-                singleLine = true)
-            SingleValidatorMessage(message)
-        }
-    }, buttons = {
-        CancelOkButtonRow(
-            okButtonEnabled = message is Validator.Message.Error,
-            onCancelClick = onDismissRequest,
-            onOkClick = onConfirm)
-    },
-)
+) {
+    if (state !is NameDialogState.Showing)
+        return
+    val title = stringResource(R.string.add_item_group_dialog_title)
+    NameDialog(title, state, modifier)
+}
