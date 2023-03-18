e2da49cbd18251bb700dfed7909c89883ac1d914
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-03-18 14:51:29 -0400

    Prevent hiding the bottom drawer via swiping

--- app/src/main/java/com/cliffracertech/bootycrate/bottomdrawer/BottomAppDrawer.kt
+++ app/src/main/java/com/cliffracertech/bootycrate/bottomdrawer/BottomAppDrawer.kt
@@ -6,6 +6,7 @@ package com.cliffracertech.bootycrate.bottomdrawer
 
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -25,11 +26,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cliffracertech.bootycrate.R
 import com.cliffracertech.bootycrate.bottombar.BootyCrateBottomAppBar
@@ -77,6 +82,14 @@ class DrawerSizes(
  * A swipeable bottom app drawer that can be hidden, collapsed, or expanded.
  *
  * @param sizes The [DrawerSizes] that describes the drawer
+ * @param allowDragToHide Whether or not hiding the drawer via swiping
+ *     down should be allowed. If false, downward drag gestures will be
+ *     blocked when the drawer is already in its collapsed state. While
+ *     this effect can also be achieved via a SwipeableState.confirmStateChange,
+ *     setting allowDragToHide to false will also prevent the drawer
+ *     from moving toward the hidden state (similar to an infinite
+ *     resistance), and will not prevent programmatic changes to the
+ *     hidden state (e.g. using swipeableState.animateTo(Hidden).
  * @param modifier The [Modifier] to use for the root layout
  * @param swipeableState A [SwipeableState]`<DrawerState>` instance
  * @param content The content of the drawer. To help you define what
@@ -87,6 +100,7 @@ class DrawerSizes(
  */
 @Composable fun BottomAppDrawer(
     sizes: DrawerSizes,
+    allowDragToHide: Boolean,
     modifier: Modifier = Modifier,
     swipeableState: SwipeableState<DrawerState> =
         rememberSwipeableState(Collapsed),
@@ -103,7 +117,21 @@ class DrawerSizes(
         resistance = ResistanceConfig(
             basis = sizes.heightChangePx / 2f,
             factorAtMin = Float.MAX_VALUE,
-            factorAtMax = Float.MAX_VALUE))
+            factorAtMax = Float.MAX_VALUE)
+    ).pointerInput(allowDragToHide) {
+        if (!allowDragToHide) forEachGesture {
+            awaitPointerEventScope {
+                while(true) {
+                    awaitPointerEvent(PointerEventPass.Initial).changes.fastForEach {
+                        val yPosChange = it.positionChange().y
+                        val offset = swipeableState.offset.value
+                        if (yPosChange > 0f && offset > sizes.heightChangePx)
+                            it.consume()
+                    }
+                }
+            }
+        }
+    }
 ) {
     content { // expansionProgressProvider = {
         if (swipeableState.offset.value <= sizes.heightChangePx)
@@ -117,9 +145,10 @@ class DrawerSizes(
  * A BottomAppDrawer with state provided by an instance of [BottomAppDrawerViewModel].
  *
  * @param modifier The [Modifier] that will apply to the entire drawer
- * @param additionalPeekHeight A [Dp] value that will be added to the default
- *     56.dp peek height of the drawer. This value can be used to adjust for
- *     the height of the bottom navigation bar, if any.
+ * @param additionalPeekHeight A [Dp] value that will be added to the
+ *     default 56.dp peek height of the drawer as well as its expanded
+ *     height. This value can be used to adjust for the height of the
+ *     bottom navigation bar, if any.
  */
 @Composable fun BootyCrateBottomAppDrawer(
     modifier: Modifier = Modifier,
@@ -134,8 +163,7 @@ class DrawerSizes(
     }
     val swipeableState = rememberSwipeableState(
         initialValue = Collapsed,
-        animationSpec = spring(stiffness = springStiffness),
-        confirmStateChange = { !it.isHidden || vm.drawerIsHidden })
+        animationSpec = spring(stiffness = springStiffness))
 
     LaunchedEffect(vm.drawerIsHidden) {
         swipeableState.animateTo(
@@ -145,6 +173,7 @@ class DrawerSizes(
 
     BottomAppDrawer(
         sizes = drawerSizes,
+        allowDragToHide = false,
         modifier = modifier,
         swipeableState = swipeableState,
     ) { expansionProgressProvider ->
